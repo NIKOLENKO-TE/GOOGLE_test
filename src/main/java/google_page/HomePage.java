5c0033e25207127562251dbeafb66fa83fb240e6
NIKOLENKO <nikolenkote@gmail.com>
2024-06-21 08:44:07 +0200

    Initial commit

---
+++ src/main/java/google_page/HomePage.java
@@ -0,0 +1,13 @@
+package google_page;
+
+import core.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+  public static final String HOME_PAGE_URL = "https://demoqa.com/";
+
+
+
+
+}
